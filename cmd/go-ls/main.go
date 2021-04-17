193789b3905bd5320aeb6269b60ff5b87233a90a
Tatsuya Teshima <terima666tatsuya@gmail.com>
2021-04-18 00:12:33 +0900

    add directory ls

--- cmd/go-ls/main.go
+++ cmd/go-ls/main.go
@@ -1,20 +1,28 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"os"
 	"strings"
 )
 
 func main() {
+	flag.Parse()
+
 	dir, err := os.Getwd()
 	if err != nil {
 		os.Exit(-1)
 	}
+	if flag.Arg(0) != "" {
+		dir = flag.Arg(0)
+	}
+
 	fileInfos, err := ioutil.ReadDir(dir)
 	if err != nil {
-		os.Exit(-1)
+		log.Fatal("No such file or directory")
 	}
 
 	for _, fileInfo := range fileInfos {
