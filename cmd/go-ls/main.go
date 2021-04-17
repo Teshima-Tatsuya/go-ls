fb9ddc281149dd2332277e8281141e6b08c44148
Tatsuya Teshima <terima666tatsuya@gmail.com>
2021-04-17 10:46:19 +0900

    first ls

---
+++ cmd/go-ls/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+func main() {
+	dir, err := os.Getwd()
+	if err != nil {
+		os.Exit(-1)
+	}
+	fileInfos, err := ioutil.ReadDir(dir)
+	if err != nil {
+		os.Exit(-1)
+	}
+
+	for _,fileInfo := range fileInfos {
+		fmt.Println(fileInfo.Name())
+	}
+}
